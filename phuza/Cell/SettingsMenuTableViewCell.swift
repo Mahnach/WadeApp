1e6a1c5cc53aba606f821ef1a1835e1d4b8e7e6b
Sorochinskiy Dmitriy <nea.dima@gmail.com>
2018-11-04 10:15:44 +0300

    Implemented privacy and policies screens

---
+++ phuza/Cell/SettingsMenuTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsMenuTableViewCell.swift
+//  phuza
+//
+//  Created by Sorochinskiy Dmitriy on 04.11.2018.
+//  Copyright © 2018 phuza. All rights reserved.
+//
+
+import UIKit
+
+class SettingsMenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellTitle: UILabel!
+    
+}
---
+++ phuza/Controllers/PrivacyViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PrivacyViewController.swift
+//  phuza
+//
+//  Created by Sorochinskiy Dmitriy on 04.11.2018.
+//  Copyright © 2018 phuza. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func closeAction() {
+        dismiss(animated: true, completion: nil)
+    }
+}
